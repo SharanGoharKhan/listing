7a5a1867d9cc07b476f4f5009216810c1511812e
saadi-ninjasCode <saadjaved79@gmail.com>
2020-09-17 18:22:12 +0500

    components index file

---
+++ listing-app/src/components/index.js
@@ -0,0 +1,13 @@
+import { FlashMessage } from './FlashMessage/FlashMessage'
+import { TextDefault, TextError } from './Text/index'
+import Spinner from './Spinner/Spinner'
+import MainHeader from './Header/MainHeader/MainHeader'
+import ModalHeader from './Header/ModalHeader/ModalHeader'
+import SearchHeader from './Header/SearchHeader/SearchHeader'
+import LocationModal from './Modal/LocationModal/LocationModal'
+import FilterModal from './Modal/FilterModal/FilterModal'
+import ReportModal from './Modal/ReportModal/ReportModal'
+import RadioBtn from './RadioBtn/RadioBtn'
+import { LeftButton, BackButton, RightButton } from './Header/HeaderIcons/HeaderIcons'
+
+export { FlashMessage, TextDefault, TextError, Spinner, LocationModal, ModalHeader, MainHeader, ReportModal, BackButton, LeftButton, RightButton, FilterModal, SearchHeader, RadioBtn }
