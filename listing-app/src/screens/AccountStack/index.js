4a82ec51362d6dbde1bd0f6478e3da4c353f9e86
saadi-ninjasCode <saadjaved79@gmail.com>
2020-09-17 19:11:53 +0500

    index file updated

--- listing-app/src/screens/AccountStack/index.js
+++ listing-app/src/screens/AccountStack/index.js
@@ -1,3 +1,6 @@
 import MainAccount from './MainAccount/MainAccount'
+import Help from './Help/Help'
+import Settings from './Settings/Settings'
+import Packages from './Packages/Packages'
 
-export { MainAccount }
+export { MainAccount, Help, Settings, Packages }
