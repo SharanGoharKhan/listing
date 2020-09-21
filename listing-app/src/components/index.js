a8085830dd62bc8b4018995a46fd642b6405d03b
saadi-ninjasCode <saadjaved79@gmail.com>
2020-09-21 20:50:21 +0500

    component index file updated

--- listing-app/src/components/index.js
+++ listing-app/src/components/index.js
@@ -8,7 +8,8 @@ import LocationModal from './Modal/LocationModal/LocationModal'
 import FilterModal from './Modal/FilterModal/FilterModal'
 import ReportModal from './Modal/ReportModal/ReportModal'
 import RadioBtn from './RadioBtn/RadioBtn'
-import EmptyButton from './Buttons/EmptyButton';
+import EmptyButton from './Buttons/EmptyButton/EmptyButton';
+import DisconnectButton from './Buttons/DisconnectButton/DisconnectButton'
 import { LeftButton, BackButton, RightButton } from './Header/HeaderIcons/HeaderIcons'
 
-export { FlashMessage, TextDefault, TextError, Spinner, LocationModal, ModalHeader, MainHeader, ReportModal, BackButton, LeftButton, RightButton, FilterModal, SearchHeader, RadioBtn, EmptyButton }
+export { FlashMessage, TextDefault, TextError, Spinner, LocationModal, ModalHeader, MainHeader, ReportModal, BackButton, LeftButton, RightButton, FilterModal, SearchHeader, RadioBtn, EmptyButton, DisconnectButton }
