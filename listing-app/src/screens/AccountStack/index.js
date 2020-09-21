aea16080e3914b43b7803fb5fc3f08ef2445af9d
saadi-ninjasCode <saadjaved79@gmail.com>
2020-09-21 20:51:33 +0500

    Screen index file updated

--- listing-app/src/screens/AccountStack/index.js
+++ listing-app/src/screens/AccountStack/index.js
@@ -4,5 +4,9 @@ import Settings from './Settings/Settings'
 import Packages from './Packages/Packages'
 import Followers from './Network/Followers/Followers'
 import Following from './Network/Following/Following'
+import Profile from './Profile/Profile'
+import EditProfile from './EditProfile/EditProfile'
+import EditPhone from './EditPhone/EditPhone'
+import EditEmail from './EditEmail/EditEmail'
 
-export { MainAccount, Help, Settings, Packages, Followers, Following }
+export { MainAccount, Help, Settings, Packages, Followers, Following, Profile, EditProfile, EditPhone, EditEmail }
