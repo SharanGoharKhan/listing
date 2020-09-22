e9ff79133d5950cf7eba8b40b647ce588593a0ee
saadi-ninjasCode <saadjaved79@gmail.com>
2020-09-22 19:31:41 +0500

    billing screeen added

---
+++ listing-app/src/screens/AccountStack/Billling/Billing.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Image, View } from 'react-native'
+import { TextDefault } from '../../../components'
+import { alignment } from '../../../utilities'
+import styles from './styles'
+
+function Billing() {
+
+    function emptyView() {
+        return (
+            <View style={[styles.flex, styles.emptyContainer]}>
+                <Image
+                    style={styles.emptyImage}
+                    source={require('../../../assets/images/emptyView/under-construction.png')}
+                />
+                <TextDefault H4 center bold style={alignment.MTlarge}>
+                    {"Comming Soon...."}
+                </TextDefault>
+                <TextDefault H5 center light style={alignment.MTsmall}>
+                    {"This feature is under-construction."}
+                </TextDefault>
+            </View>
+        )
+    }
+
+    return (
+        <View style={[styles.flex, styles.mainContainer]}>
+            {emptyView()}
+        </View>
+    )
+}
+export default React.memo(Billing)
