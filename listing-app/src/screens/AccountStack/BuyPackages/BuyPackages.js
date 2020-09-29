8062cbb23686fef8647abf075b09da6398209b12
saadi-ninjasCode <saadjaved79@gmail.com>
2020-09-29 14:08:44 +0500

    buy packages screen added

--- listing-app/src/screens/AccountStack/BuyPackages/BuyPackages.js
+++ listing-app/src/screens/AccountStack/BuyPackages/BuyPackages.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import { Image, View } from 'react-native'
-import { TextDefault } from '../../../components'
-import { alignment } from '../../../utilities'
+import React, { useState } from 'react'
+import { Image, View, TouchableOpacity } from 'react-native'
+import { TextDefault, EmptyButton, LocationModal } from '../../../components'
+import { alignment, scale, colors } from '../../../utilities'
 import styles from './styles'
+import { Entypo } from '@expo/vector-icons'
+import { useNavigation, useRoute } from '@react-navigation/native'
+
+const empty = false
 
 function BuyPackages() {
+    const navigation = useNavigation()
+    const routes = useRoute()
+    const showButton = routes.params?.button ?? true
+    const [filters, setFilters] = useState({})
+    const [modalVisible, setModalVisible] = useState(false);
+
+    function toggleModal() {
+        setModalVisible(prev => !prev)
+    }
 
     function emptyView() {
         return (
@@ -25,7 +38,48 @@ function BuyPackages() {
 
     return (
         <View style={[styles.flex, styles.mainContainer]}>
-            {emptyView()}
+            {empty ? emptyView() :
+                <View style={[styles.flex, styles.subContainer]}>
+                    <View style={styles.flex}>
+                        <TextDefault textColor={colors.fontMainColor} H4 uppercase style={alignment.MBlarge}>
+                            {'SELECT OPTIONS TO SHOW PACKAGES'}
+                        </TextDefault>
+                        <TouchableOpacity style={styles.smallContainer}
+                            onPress={() => navigation.navigate('Categories')}>
+                            <View style={[styles.flex]}>
+                                <TextDefault bold H5 style={alignment.PLlarge}>
+                                    {'Categories'}
+                                </TextDefault>
+                                <TextDefault light style={[alignment.PLlarge, alignment.MTxSmall]}>
+                                    {showButton ? 'Search Category' : 'Mobiles'}
+                                </TextDefault>
+                            </View>
+                            <Entypo name="chevron-small-right" size={scale(30)} color={colors.buttonbackground} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.smallContainer}
+                            onPress={toggleModal}>
+                            <View style={[styles.flex]}>
+                                <TextDefault bold H5 style={alignment.PLlarge}>
+                                    {'Location'}
+                                </TextDefault>
+                                <TextDefault light style={[alignment.PLlarge, alignment.MTxSmall]}>
+                                    {'Service Society E11/2 '}
+                                </TextDefault>
+                            </View>
+                            <Entypo name="chevron-small-right" size={scale(30)} color={colors.buttonbackground} />
+                        </TouchableOpacity>
+
+                    </View>
+                    <View style={styles.buttonView}>
+                        <EmptyButton
+                            disabled={showButton}
+                            title='Show Packages'
+                            onPress={() => navigation.navigate('ShowPackages')} />
+                    </View>
+                </View>
+            }
+            <LocationModal visible={modalVisible} onModalToggle={toggleModal}
+                setFilters={setFilters} />
         </View>
     )
 }
--- listing-app/src/screens/AccountStack/BuyPackages/styles.js
+++ listing-app/src/screens/AccountStack/BuyPackages/styles.js
@@ -8,17 +8,36 @@ const styles = StyleSheet.create({
     mainContainer: {
         backgroundColor: colors.themeBackground
     },
+    subContainer: {
+        backgroundColor: colors.themeBackground,
+        ...alignment.PLmedium,
+        ...alignment.PRmedium,
+        ...alignment.MTlarge
+    },
     emptyContainer: {
         backgroundColor: colors.containerBox,
         justifyContent: "center",
         alignItems: "center",
         ...alignment.PLmedium,
-        ...alignment.PRmedium
+        ...alignment.PRmedium,
     },
     emptyImage: {
         width: scale(150),
         height: scale(150)
     },
+    smallContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "space-between",
+        borderBottomColor: colors.horizontalLine,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        ...alignment.PTmedium,
+        ...alignment.PBmedium,
+    },
+    buttonView: {
+        width: "100%",
+        ...alignment.PBsmall
+    }
 
 })
 export default styles
