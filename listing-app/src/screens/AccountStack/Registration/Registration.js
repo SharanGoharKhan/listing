69251b86ba13eacc923808dd5213d0061d4ea4ca
saadi-ninjasCode <saadjaved79@gmail.com>
2020-10-19 17:27:04 +0500

    icon for login screen

--- listing-app/src/screens/AccountStack/Registration/Registration.js
+++ listing-app/src/screens/AccountStack/Registration/Registration.js
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native'
 import React, { useContext } from 'react'
-import { View } from 'react-native'
+import { View, Image } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { ModalHeader, TextDefault } from '../../../components'
 import LoginButton from '../../../components/Buttons/LoginButton/LoginButton'
@@ -8,6 +8,7 @@ import UserContext from '../../../context/user'
 import { colors } from '../../../utilities'
 import styles from './styles'
 
+const icon = require('../../../assets/icon.png')
 
 function Registration() {
     const navigation = useNavigation()
@@ -22,9 +23,12 @@ function Registration() {
             <View style={[styles.flex, styles.mainContainer]}>
                 <ModalHeader closeModal={() => navigation.goBack()} />
                 <View style={styles.logoContainer}>
-                    <TextDefault textColor={colors.buttonbackground} bolder H1 style={styles.textTitle} >
-                        {'OLO'}
-                    </TextDefault>
+                    <View style={styles.image}>
+                        <Image
+                            source={icon}
+                            style={styles.imgResponsive}
+                            resizeMode='contain' />
+                    </View>
 
                 </View>
                 <View style={styles.buttonContainer}>
