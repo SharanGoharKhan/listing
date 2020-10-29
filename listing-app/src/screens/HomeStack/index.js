af4a8ba841b565c1c02e0a494c97125de211d784
saadi-ninjasCode <saadjaved79@gmail.com>
2020-10-29 14:49:16 +0500

    ..

--- listing-app/src/screens/HomeStack/index.js
+++ listing-app/src/screens/HomeStack/index.js
@@ -6,5 +6,6 @@ import ProductDescription from './ProductDescription/ProductDescription'
 import Notifications from './Notifications/Notifications'
 import UserProfile from './UserProfile/UserProfile'
 import FilterModal from './FilterModal/FilterModal'
+import FullMap from './FullMap/FullMap'
 
-export { MainHome, Categories, FilterModal, SubCategories, ProductListing, ProductDescription, Notifications, UserProfile }
+export { MainHome, Categories, FilterModal, SubCategories, ProductListing, ProductDescription, Notifications, UserProfile, FullMap }
